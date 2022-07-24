bcf9274ba6d7179f18c0d47ee96a8daeaad9fa0e
saliyvls <saliyvls@yandex.ru>
2022-07-24 15:17:48 +0300

    server auth handling

--- server/index.js
+++ server/index.js
@@ -1,21 +1,35 @@
 'use strict';
+
 const fastify = require('fastify')();
 
 fastify.register(require('@fastify/websocket'), {
   options: { maxPayload: 1048576 },
 });
 
+class Action {
+  method;
+  payload;
+  message;
+}
+
 fastify.register(async function (fastify) {
-  fastify.get(
-    '/',
-    { websocket: true },
-    (connection /* SocketStream */, req /* FastifyRequest */) => {
-      connection.socket.on('message', (message) => {
-        message = JSON.parse(message);
-        connection.socket.send('hi from server');
+  fastify.get('/', { websocket: true }, (connection, req) => {
+    connection.socket.on('message', (message) => {
+      message = JSON.parse(message);
+      switch (message.method) {
+        case 'connection':
+          authHanlder(connection.socket, message);
+      }
+      fastify.websocketServer.clients.forEach(function each(client) {
+        if (
+          client.readyState === 1 &&
+          client.id !== message.user.id
+        ) {
+          client.send(JSON.stringify(message));
+        }
       });
-    }
-  );
+    });
+  });
 });
 
 fastify.listen({ port: 5000 }, (err, address) => {
@@ -25,3 +39,9 @@ fastify.listen({ port: 5000 }, (err, address) => {
   }
   console.log(`Server listening at: ${address}`);
 });
+
+function authHanlder(wsClient, wsData) {
+  if (wsData.user && wsData.user.id) {
+    wsClient.id = wsData.user.id;
+  }
+}
