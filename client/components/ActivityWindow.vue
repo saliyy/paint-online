1a0c021481e77ab3707a37b9abdb39b2567e30c7
saliyvls <saliyvls@yandex.ru>
2022-08-06 21:14:04 +0300

    add overflow on activity window

--- client/components/ActivityWindow.vue
+++ client/components/ActivityWindow.vue
@@ -4,11 +4,28 @@ const actionMessagesState = useActionsMessagesState();
 </script>
 
 <template>
- <div>
-    <ul>
-        <li v-for="message in actionMessagesState.messagesShowInActivity" :key="message.id">
+ <div class="dialog">
+    <ul class="dialog__list">
+        <li class="dialog__message" v-for="message in actionMessagesState.messagesShowInActivity" :key="message.id">
             {{ message.createdAt }}: {{ message.text }}
+            <hr class="line">
         </li>
     </ul>
  </div>
 </template>
+
+<style lang="scss" scoped>
+    .dialog {
+        height: 100%;
+        overflow-y : auto;
+        &__message {
+            margin-left: 4px;
+        }
+    }
+
+    .line {
+        width: 90%;
+        border: none;
+        border: 0.5px solid lightgrey;
+    }
+</style>
